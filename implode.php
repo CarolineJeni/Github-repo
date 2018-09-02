ff3449410088b176878a37dae41ea80f59c87063
Caroline <coolcaroline1996@gmail.com>
2018-09-02 19:57:22 +0530

    Basic implode example added

---
+++ implode.php
@@ -0,0 +1,12 @@
+<html>
+<h3>Implode Function</h3>
+<?php
+$a = array ('I','am','a','girl!');
+echo implode(" ",$a);
+echo implode(" , ", $a);
+echo implode(" / ", $a);
+echo implode(" . ", $a);
+echo implode(" - ", $a);
+?>
+</body>
+</html>
